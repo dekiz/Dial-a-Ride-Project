574a4a8e60915c973c2633b892677ab4009ea938
dileka <dileka@KVM929-PC007PQY>
2016-01-24 08:30:23 +0200

    New Commit : Duration between nodes i and j has been implemented on the code so that speed of each vehicle is 30 km/h., DA, 23.01.16. Delivery times are indicated., DA, 23.01.16. Data is taken from the excel file. DA, 24.01.16.

--- src/kapgelproject/ExcelRead.java
+++ src/kapgelproject/ExcelRead.java
@@ -3,95 +3,151 @@
  * and open the template in the editor.
  */
 package kapgelproject;
+
 import java.io.File;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import jxl.Cell;
 import jxl.Sheet;
 import jxl.Workbook;
+import org.apache.commons.lang3.ArrayUtils;
 
-public class ExcelRead{
-    
-    public static String[] ExcelRead(String excel) throws Exception{
-    
-//    File f=new File("C:\\Users\\asus\\Documents\\MATLAB\\testsample.xls");
-    File f = new File(excel);
-    
-    Workbook wb = Workbook.getWorkbook(f);
-    Sheet s=wb.getSheet(0);
-    
-    int col=s.getColumns();
-    int row =s.getRows();
-    
-    double DestinationLat[]=new double[row];
-    double DestinationLong[]=new double[row];
-    double OriginLat[]=new double[row];
-    double OriginLong[]=new double[row];
-    String timestamp[]=new String[row];
-    
-    for (int j=1; j<row; j++)
-    {
-        Cell A=s.getCell(2,j);
-        String stringA = A.getContents();
+
+public class ExcelRead {
+
+    public static String[] TimesRead(String excel) throws Exception {
+
+//    File f=new File("C:\\Users\\IBM_ADMIN\\Documents\\NetBeansProjects\\KapgelProject\\KapgelProject\\simulated_data.xlsx");
+        File f = new File(excel);
+        Workbook wb = Workbook.getWorkbook(f);
+        Sheet s = wb.getSheet(0);
+
+        int col = s.getColumns();
+        int row = s.getRows();
+
+        double DestinationLat[] = new double[row];
+        double DestinationLong[] = new double[row];
+        double OriginLat[] = new double[row];
+        double OriginLong[] = new double[row];
+        String timestamp[] = new String[row];
+
+        for (int j = 1; j < row; j++) {
+            Cell A = s.getCell(2, j);
+            String stringA = A.getContents();
+//            System.out.println(stringA.substring(0, stringA.length()));
+            DestinationLat[j] = Double.parseDouble(stringA);
+
+            Cell B = s.getCell(3, j);
+            String stringB = B.getContents();
+            DestinationLong[j] = Double.parseDouble(stringB);
+//            System.out.println(DestinationLong[j]);
+
+            Cell C = s.getCell(4, j);
+            String stringC = C.getContents();
 //        System.out.println(stringA.substring(0, stringA.length()));
-        DestinationLat[j]=Double.parseDouble(stringA);
-        
-               
-        Cell B=s.getCell(3,j);
-        String stringB = B.getContents();
-        
-        DestinationLong[j]=Double.parseDouble(stringB);
-//        System.out.println(DestinationLong[j]);
-        
-        Cell C=s.getCell(4,j);
-        String stringC = C.getContents();
-               
-//        System.out.println(stringA.substring(0, stringA.length()));
-        OriginLat[j]=Double.parseDouble(stringC);
-        
-        Cell D=s.getCell(5,j);
-        String stringD = D.getContents();
-        
-        OriginLong[j]=Double.parseDouble(stringD);
-        
-        
-        // read timestamp
-        
-        Cell E = s.getCell(1,j);
-        String stringE=E.getContents();
-        timestamp[j]=stringE;
-       
-        
-        
+            OriginLat[j] = Double.parseDouble(stringC);
 
-    }
-    
-        String times[]=new String[row-1];
-        for (int i=1; i<row; i++){
-            times[i-1]=timestamp[i];
-        }
-    
-        double coordinates[] = new double [4*row-4];
-        for (int i=1; i<row; i++){
-            coordinates[i-1]=DestinationLat[i];
-        }
-        for (int i=1; i<row; i++){
-            coordinates[i+row-2]=DestinationLong[i];
-        }
-        for (int i=1; i<row; i++){
-            coordinates[i+2*row-3]=OriginLat[i];
+            Cell D = s.getCell(5, j);
+            String stringD = D.getContents();
+            OriginLong[j] = Double.parseDouble(stringD);
+
+            // read timestamp
+            Cell E = s.getCell(1, j);
+            String stringE = E.getContents();
+            timestamp[j] = stringE;
+//            System.out.println(timestamp[j]);
         }
 
-        for (int i=1; i<row; i++){
-            coordinates[i+3*row-4]=OriginLong[i];
+        String times[] = new String[row - 1];
+        for (int i = 1; i < row; i++) {
+            times[i - 1] = timestamp[i];
         }
 
-        for (int i=0; i<coordinates.length; i++){
+        double coordinates[] = new double[4 * row - 4];
+        for (int i = 1; i < row; i++) {
+            coordinates[i - 1] = DestinationLat[i];
+        }
+        for (int i = 1; i < row; i++) {
+            coordinates[i + row - 2] = DestinationLong[i];
+        }
+        for (int i = 1; i < row; i++) {
+            coordinates[i + 2 * row - 3] = OriginLat[i];
+        }
+
+        for (int i = 1; i < row; i++) {
+            coordinates[i + 3 * row - 4] = OriginLong[i];
+        }
+
+        for (int i = 0; i < coordinates.length; i++) {
 //            System.out.println(coordinates[i]);
         }
 
-return times;
+        return times;
+
+    }
+
+     public static double[][] CoordinatesRead(String excel) throws Exception {
+
+//    File f=new File("C:\\Users\\IBM_ADMIN\\Documents\\NetBeansProjects\\KapgelProject\\KapgelProject\\simulated_data.xlsx");
+        File f = new File(excel);
+        Workbook wb = Workbook.getWorkbook(f);
+        Sheet s = wb.getSheet(0);
+
+        int col = s.getColumns();
+        int row = s.getRows();
+
+        double DestinationLat[] = new double[row];
+        double DestinationLong[] = new double[row];
+        double OriginLat[] = new double[row];
+        double OriginLong[] = new double[row];
+
+        for (int j = 1; j < row; j++) {
+            Cell A = s.getCell(2, j);
+            String stringA = A.getContents();
+//            System.out.println(stringA.substring(0, stringA.length()));
+            DestinationLat[j] = Double.parseDouble(stringA);
+            
+            Cell B = s.getCell(3, j);
+            String stringB = B.getContents();
+            DestinationLong[j] = Double.parseDouble(stringB);
+//            System.out.println(DestinationLong[j]);
+
+            Cell C = s.getCell(4, j);
+            String stringC = C.getContents();
+//        System.out.println(stringC.substring(0, stringC.length()));
+            OriginLat[j] = Double.parseDouble(stringC);
+
+            Cell D = s.getCell(5, j);
+            String stringD = D.getContents();
+            OriginLong[j] = Double.parseDouble(stringD);
+
+        }
+        
+        double pickupcoordinates[][] = {DestinationLat,DestinationLong};
+        double [][] coordinates = new double[2*(pickupcoordinates[0].length-1)+2][2];
+         coordinates[0][0] = 41.041999;
+         coordinates[0][1] = 28.961532;
+         coordinates[2*(pickupcoordinates[0].length-1)+1][0] = 41.041999;
+         coordinates[2*(pickupcoordinates[0].length-1)+1][1] = 28.961532;
+        for (int i = 1; i < pickupcoordinates[0].length; i++){
+         for (int j=0; j<2; j++){
+             coordinates[i][0] = DestinationLat[i];
+             coordinates[i][1] = DestinationLong[i];
+             coordinates[i+pickupcoordinates[0].length-1][0]=OriginLat[i];
+             coordinates[i+pickupcoordinates[0].length-1][1]=OriginLong[i];
+
+             
+         }
+     }
+//        
+//        for (int i=0; i< (2*(pickupcoordinates[0].length-1)+2); i++){
+//            for (int j=0; j<2; j++){
+//                System.out.println(test[i][j]);
+//            }
+//        }
+        
+        return coordinates;
+
+    }
+
 
 }
 
-}
--- src/kapgelproject/KapgelProject.java
+++ src/kapgelproject/KapgelProject.java
@@ -5,13 +5,13 @@ public class KapgelProject {
 
     public static void main(String[] args) throws Exception {
         Darp.SolveMe();
-//        ExcelRead.ExcelRead("C:\\Users\\asus\\Documents\\MATLAB\\testsample.xls");
-//        double coordinates[]=ExcelRead.ExcelRead("C:\\Users\\asus\\Documents\\MATLAB\\testsample.xls");
-//        for (int i=0; i<coordinates.length;i++){
-////            System.out.println(coordinates[i]);
+//        ExcelRead.ExcelRead("C:\\Users\\IBM_ADMIN\\Documents\\NetBeansProjects\\KapgelProject\\KapgelProject\\simulated_data.xls");
+//        String times[]=ExcelRead.TimesRead("C:\\Users\\IBM_ADMIN\\Documents\\NetBeansProjects\\KapgelProject\\KapgelProject\\simulated_data.xls");
+//        for (int i=0; i<times.length;i++){
+//            System.out.println(times[i]);
 //        }
-        
-        
+          
+
     }
     
     
